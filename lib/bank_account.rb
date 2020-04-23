5a22a1c8b0f2d41a3b85d52edee83264d30cad05
ewelinaszoda <ewelinaszoda@gmail.com>
2020-04-23 16:56:19 +0200

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
+# cannot change the name, just read 
+require 'pry'
 class BankAccount
 
-end
+    attr_accessor :balance, :status
+     
+    attr_reader :name
+   
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(add_money)
+        @balance = @balance + add_money
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid? 
+        @status == "open" && @balance > 0
+    end
+
+    def close_account
+        @status = "closed"
+    end
+#binding.pry
+end 
