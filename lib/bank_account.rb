55648725fb05be07e51cdddda83b920feae78a80
zbadiru <zbadiru@live.co.uk>
2020-04-23 21:15:52 +0100

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
+# pair coded with Zakary Bad-Iru & Jamie Symonds-Tayler
+require 'pry'
 class BankAccount
+    
+    def initialize(name, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
 
+    attr_reader :name, :balance, :status
+    attr_writer :balance, :status
+    
+    def deposit(amount)
+        @balance += amount
+    end
+    
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+    
+    def valid?
+       @status == "open" && @balance > 0            
+    end
+    
+    def close_account
+        @status = "closed"
+    end
 end
