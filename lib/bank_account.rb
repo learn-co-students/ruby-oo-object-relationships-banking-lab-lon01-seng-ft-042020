18c80eb58a904d55ac8be725ba2e344af1b9a359
github username <github email address>
2020-04-24 11:38:06 +0100

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,24 +1,36 @@
 class BankAccount
+  #Because there is no writer for name, it cant be changed
     attr_reader :name
+  #In valid? you need to be able to write to balance & status
     attr_accessor :balance, :status
 
+  #Can initialize a Bank Account
+  #Initializes with a name, that cant be changed
+  #Always initializes with balance of 1000
+  #Always initializes with a status of 'open'
+  # NOTE: You can set default values in initialize 
     def initialize(name)     
       @name = name       
       @balance = 1000        
       @status = "open"    
     end
 
+  #Can use @balance, balance & self.balance interchangeably
     def deposit(amount)      
     self.balance += amount  
     end
-
+     
+  #Can use @balance, balance & self.balance interchangeably                          
     def display_balance        
      "Your balance is $#{@balance}."   
     end
 
+    #Can use @balance, balance & self.balance interchangeably
+    #Similarly with @status, status & self.status
+    #Use ternary operator for readability (non-programmers)
     def valid?    
     self.status == "open" && self.balance > 0  
-     end  
+    end  
     
     def close_account        
         self.status = "closed"   
